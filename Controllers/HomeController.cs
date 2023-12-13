4830a81283d1ed8c0d1b3a07d717e30e36e23289
Kenneth Okalang <okalang.ok@gmail.com>
2023-12-13 08:20:54 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new MessageModel
+            {
+                Message = "Hello from DevOps Kurs!"
+            };
+
+            return View(model);
+        }
+    }
+}
+
---
+++ Models/MessageModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleApp.Models
+{
+    public class MessageModel
+    {
+        public string Message { get; set; }
+    }
+}
